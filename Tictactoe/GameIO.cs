6f9ba94e3af8407cb92162910cffd7f1e47a34ba
Roya.T <32629566+Roya-myob@users.noreply.github.com>
2021-10-06 14:58:27 +1100

    new class boardSquare added to model the board with coord and value. Used TDD for this.

---
+++ Tictactoe/GameIO.cs
@@ -0,0 +1,7 @@
+﻿namespace Tictactoe
+{
+    public class GameIO
+    {
+        
+    }
+}
---
+++ TictactoeTests/BoardSquareTest.cs
@@ -0,0 +1,7 @@
+﻿namespace TictactoeTests
+{
+    public class BoardSquareTest
+    {
+        
+    }
+}
